2f496fb78fd8438b64133ddf05ae2f413456a2fd
Braxton2719 <oltermanb@etsu.edu>
2025-04-02 23:35:18 -0400

    Game Submission Get test

--- BucStop_Snake/Snake/obj/Debug/net6.0/Snake.AssemblyInfo.cs
+++ BucStop_Snake/Snake/obj/Debug/net6.0/Snake.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3880eb9de7b41adb13ebdbec4196eeff8d2780ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BucStop_Tetris/Tetris/obj/Release/net6.0/Tetris.AssemblyInfo.cs
+++ BucStop_Tetris/Tetris/obj/Release/net6.0/Tetris.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3880eb9de7b41adb13ebdbec4196eeff8d2780ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
